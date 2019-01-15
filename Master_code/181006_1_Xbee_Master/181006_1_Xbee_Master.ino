974360c277e3639630b09b82fbbe5162cf9311e9
Arenillas <ca.arenillas@gmail.com>
2019-01-16 00:44:54 +0100

    Master Code updated to include a better example

--- Master_code/181006_1_Xbee_Master/181006_1_Xbee_Master.ino
+++ Master_code/181006_1_Xbee_Master/181006_1_Xbee_Master.ino
@@ -29,11 +29,18 @@ uint8_t TotalStepsB = 0;
 uint8_t CoolingB = 0;
 uint8_t SparkingB = 0;
 //uint8_t payload[] = {StickA, EffectA, RedA, GreenA, BlueA, Interval1A, Interval2A, TotalStepsA, CoolingA, SparkingA};
-uint8_t payload1[] = {StickA, 1, 100, 0, 0, 100, 100, 100, CoolingA, SparkingA};
-uint8_t payload2[] = {StickA, BREATHE, 0, 100, 0, 30, 8, 100, CoolingA, SparkingA, StickB, EffectB, 0, 0, 100, Interval1B, Interval2B, TotalStepsB, CoolingB, SparkingB};
-uint8_t payload3[] = {StickB, EffectB, 0, 0, 0, Interval1B, Interval2B, TotalStepsB, CoolingB, SparkingB};
-uint8_t payload4[] = {StickA, EffectA, 0, 0, 100, Interval1A, Interval2A, TotalStepsA, CoolingA, SparkingA, StickB, EffectB, 0, 0, 0, Interval1B, Interval2B, TotalStepsB, CoolingB, SparkingB};
-uint8_t payload5[] = {StickA, BREATHE, 0, 100, 0, 30, 8, 100, CoolingA, SparkingA, StickA, BREATHE, 0, 0, 100, 30, 8, 100, CoolingA, SparkingA, StickB, EffectB, 0, 0, 100, Interval1B, Interval2B, TotalStepsB, CoolingB, SparkingB};
+uint8_t payload1[] = {StickA, FULL_COLOR, 100, 0, 0, 0, 0, 0, 0, 0, StickB, FULL_COLOR, 0, 100, 0, 0, 0, 0, 0, 0};
+uint8_t payload1a[] = {StickA, FULL_COLOR, 0, 0, 0, 0, 0, 0, 0, 0, StickB, FULL_COLOR, 0, 0, 0, 0, 0, 0, 0, 0};
+uint8_t payload2[] = {StickA, DROP, 0, 0, 100, 50, 0, 0, 0, 0, StickB, DROP, 0, 0, 100, 100, 0, 0, 0, 0};
+uint8_t payload3[] = {StickA, DOUBLE_DROP, 100, 0, 0, 100, 0, 0, 0, 0, StickB, DOUBLE_DROP, 0, 100, 0, 100, 0, 0, 0, 0};
+uint8_t payload5[] = {StickA, SPARKLE, 100, 0, 0, 1, 0, 0, 0, 0, StickB, SPARKLE, 0, 100, 0, 1, 0, 0, 0, 0};
+uint8_t payload6[] = {StickA, STROBE, 100, 0, 0, 255, 255, 255, 0, 0, StickB, STROBE, 0, 100, 0, 255, 255, 255, 0, 0};
+uint8_t payload7[] = {StickA, FADE, 0, 0, 100, 50, 50, 100, 0, 0, StickB, FADE, 100, 0, 0, 50, 50, 100, 0, 0};
+uint8_t payload8[] = {StickA, RAINBOW, 0, 0, 100, 100, 0, 40, 0, 0, StickB, RAINBOW, 100, 0, 0, 50, 0, 20, 0, 0};
+uint8_t payload9[] = {StickA, SWEEP, 100, 0, 0, 60, 50, 100, 0, 0, StickB, SWEEP, 0, 100, 0, 60, 50, 100, 0, 0};
+uint8_t payload10[] = {StickA, FIRE_H, 100, 0, 0, 100, 50, 100, 0, 0, StickB, FIRE_H, 0, 100, 0, 100, 50, 100, 0, 0};
+uint8_t payload11[] = {StickA, FIRE_V, 100, 0, 0, 100, 5, 100, 55, 120, StickB, FIRE_V, 0, 100, 0, 100, 2, 100, 55, 120};
+uint8_t payload12[] = {StickA, BREATHE, 0, 0, 100, 20, 0, 100, 0, 0, StickB, BREATHE, 100, 0, 0, 20, 0, 100, 0, 0};
 
 
 void setup(){  
@@ -49,13 +56,29 @@ void setup(){
 void loop(){
   //Example of loop
   delay(3000);
-  eManager.Change(Slave_address, payload1, 10);
+  eManager.Change(Slave_address, payload1, 20);
   delay(5000);
   eManager.Change(Slave_address, payload2, 20);
   delay(5000);
-  eManager.Change(Slave_address, payload3, 10);
+  eManager.Change(Slave_address, payload3, 20);
   delay(5000);
-  eManager.Change(Slave_address, payload5, 30);
+  eManager.Change(Slave_address, payload5, 20);
+  delay(5000);
+  eManager.Change(Slave_address, payload6, 20);
+  delay(5000);
+  eManager.Change(Slave_address, payload1, 20);
+  delay(1000);
+  eManager.Change(Slave_address, payload7, 20);
+  delay(5000);
+  eManager.Change(Slave_address, payload8, 20);
+  delay(5000);
+  eManager.Change(Slave_address, payload9, 20);
+  delay(5000);
+  eManager.Change(Slave_address, payload10, 20);
+  delay(5000);
+  eManager.Change(Slave_address, payload11, 20);
+  delay(5000);
+  eManager.Change(Slave_address, payload12, 20);
   while(1);  
 }
 
